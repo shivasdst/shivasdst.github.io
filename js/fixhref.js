ed5f99018bdb36b8f6554ff35dc4f642612b0bdc
shivasdst <shivably04sdst@gmail.com>
2015-03-17 15:47:39 +0530

    Commit No. 4 - Added search box in index.html

--- js/fixhref.js
+++ js/fixhref.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
 
+var availableTags = [
+"abac",
+"abaca",
+"Abaddon",
+"abandoned",
+"abandonee",
+"abandoner",
+"abandonment",
+"abase",
+"abasement",
+"abash",
+"abashment",
+"abasia",
+"abask",
+"abate",
+"abatement",
+"abatis",
+"abatised",
+"abattis",
+"abattoir",
+"abaxial",
+"abaxile",
+"abaya",
+"abb",
+"Abba",
+"babe",
+"babel",
+"babelise",
+"babelism",
+"babelize",
+"Babi",
+"babirusa",
+"Babism",
+"Babist",
+"baboo",
+"baboon",
+"baboonery",
+"babouche",
+"babu",
+"babul",
+"babushka"
+];
+
 var headerHeight = $("header").height();
 //var headerHeight = 90;
 
@@ -16,5 +59,20 @@ var headerHeight = $("header").height();
         });
 	});
 
+$("#select_word").autocomplete({
+      source: availableTags
+    });
+
+$('#form_data').on('submit', function(e) { //use on if jQuery 1.7+
+        e.preventDefault();  //prevent form from submitting
+        var data = $('#select_word').val();
+      
+        var alpha = data.split("");
+                
+        var url = "http://localhost/Dictionary_online/src/"+ alpha[0].toUpperCase() + "/html/" + alpha[0].toLowerCase() + "1_uni.html#" + data;
+        //alert(url);
+		window.location.replace(url);
+    });
 
 });
+
